f6c19dd72afab2436a688622c6f2b2c362f74e07
ankur8991 <kumarankur5956@gmail.com>
2025-09-02 18:53:56 +0530

    Revert "Practice 85: Add Node.js backend intro"
    This reverts commit b7926c6e95eb153ad84dd6d3351a0d55bf5a47ff.

--- Node.JS/practice 85/server.js
+++
@@ -1,8 +0,0 @@
-var slugify = require('slugify');
-
-let a = slugify('some string');
-console.log(a);
-
-// if you prefer something other than dash (-) as separator.
-const b = slugify('some st#%#^$^#^^&ring' , '_');
-console.log(b);
